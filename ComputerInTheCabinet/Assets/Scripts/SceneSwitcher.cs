0984338758b399b5acd57b441a3abb2a541c158b
Alex Dawson <aad4wson@gmail.com>
2020-11-09 14:22:14 -0800

    hi

---
+++ ComputerInTheCabinet/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToFirstScene()
+    {
+        SceneManager.LoadScene("SceneOne");
+    }
+}
